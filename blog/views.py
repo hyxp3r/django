19e8660944e758c2c5855f361547eadd3475e492
Ermakov <e.n.ermakov@nsuem.ru>
2022-10-14 14:54:57 +0700

    Create back for url

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
 # Create your views here.
+
+def post_list(request):
+    posts = Post.objects.order_by('title')
+    return render(request, 'blog/post_list.html', {'posts': posts})
