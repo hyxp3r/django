8e96b5c25a235409330d5194e1204db12dd2fa24
Ermakov <e.n.ermakov@nsuem.ru>
2022-10-15 14:37:07 +0700

    Add url to post_detail

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,10 @@
 from django import views
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.post_list, name = 'post_list')
-]
+    path('', views.post_list, name = 'post_list'),
+    path('post/<int:pk>/', views.post_detail, name = 'post_detail'),
+] 
