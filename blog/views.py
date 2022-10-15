67a10d531466308e8720ffd506d9340c93f31d23
Ermakov <e.n.ermakov@nsuem.ru>
2022-10-15 14:37:37 +0700

    Add view to url post_detail

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Post
 from django.utils import timezone
 
@@ -7,3 +7,9 @@ from django.utils import timezone
 def post_list(request):
     posts = Post.objects.order_by('title')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+
+    post = get_object_or_404(Post, pk = pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
